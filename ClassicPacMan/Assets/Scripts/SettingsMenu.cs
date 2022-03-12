1565dc665f1eee88259ba096b0d1efdea0a362f5
Rudi Wagner <rudi.wagner62@gmail.com>
2022-03-12 22:35:29 +0100

    added more settings

--- ClassicPacMan/Assets/Scripts/SettingsMenu.cs
+++ ClassicPacMan/Assets/Scripts/SettingsMenu.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
     public int controlMode { get; private set; }
     public Toggle joystickToggle;
     public Toggle arrowKeysToggle;
+    private int resetCNT = 0;
+    public TextMeshProUGUI resetText;
 
     private void Start()
     {
@@ -37,5 +40,28 @@ public class SettingsMenu : MonoBehaviour
     public void ToggleArrowKeys()
     {
         PlayerPrefs.SetInt("Controls", 1);
+    }
+
+    public void ResetData()
+    {
+        resetCNT++;
+        if (resetCNT >= 2)
+        {
+            PlayerPrefs.DeleteAll();
+            resetCNT = 0;
+            this.resetText.text = "Done! No more Data!";
+            CancelInvoke();
+            Invoke(nameof(ResetText), 3.0f);
+        }
+        else
+        {
+            this.resetText.text = "Click again, if you are sure!";
+            Invoke(nameof(ResetText), 3.0f);
+        }
+    }
+
+    private void ResetText()
+    {
+        this.resetText.text = "Reset Game Data";
     }
 }
